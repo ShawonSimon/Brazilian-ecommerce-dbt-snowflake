8888e1e5b26266c949280f818f04e38ad36e39e4
Shawon Simon <shawonsymon@gmail.com>
2025-01-24 01:21:56 +0100

    dim_order_keys corrected

--- brazilian_ecommerce_dbt_snowflake/models/marts/dim_order_keys.sql
+++ brazilian_ecommerce_dbt_snowflake/models/marts/dim_order_keys.sql
@@ -1,7 +1,7 @@
 WITH order_keys AS (
     SELECT
         order_id,
-        {{ dbt_utils.generate_surrogate_key(['order_id']) }} AS order_key
+        ROW_NUMBER() OVER (ORDER BY order_id) AS order_key
     FROM {{ ref('stg_orders') }}
 )
 
