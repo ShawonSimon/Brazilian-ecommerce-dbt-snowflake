3a591807aaf88a1fee5d801933b88b3d47c5dcd8
Shawon Simon <shawonsymon@gmail.com>
2025-01-24 20:41:02 +0100

    product dimension without english translation

--- brazilian_ecommerce_dbt_snowflake/models/marts/dimProduct.sql
+++ brazilian_ecommerce_dbt_snowflake/models/marts/dimProduct.sql
@@ -1,1 +1,8 @@
 SELECT
+    dim_product_keys.product_key,
+    stg_products.product_id,
+    stg_products.product_category_name
+FROM {{ ref('dim_product_keys') }}
+LEFT JOIN {{ ref('stg_products') }} ON
+    dim_product_keys.product_id = stg_products.product_id
+ORDER BY dim_product_keys.product_key
