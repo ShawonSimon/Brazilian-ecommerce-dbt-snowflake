07816396b5f9e438a4aaf1ef18145e7531515575
Shawon Simon <shawonsymon@gmail.com>
2025-01-25 05:02:05 +0100

    create dim seller dimension

--- brazilian_ecommerce_dbt_snowflake/models/marts/dimSellers.sql
+++ brazilian_ecommerce_dbt_snowflake/models/marts/dimSellers.sql
@@ -1,12 +1,24 @@
+WITH seller_keys AS (
+  SELECT
+    stg_sellers.seller_id,
+    ROW_NUMBER() OVER (ORDER BY stg_sellers.seller_id) AS seller_key
+  FROM {{ ref('stg_sellers') }}
+)
 SELECT
-    dim_seller_keys.seller_key,
-    dim_seller_keys.seller_id,
-    stg_sellers.seller_city,
-    stg_sellers.seller_state,
-    stg_order_items.order_id
-FROM {{ ref('dim_seller_keys') }}
-LEFT JOIN {{ ref('stg_sellers') }} ON
-    dim_seller_keys.seller_id = stg_sellers.seller_id
-LEFT JOIN {{ ref('stg_order_items') }} ON
-    dim_seller_keys.seller_id = stg_order_items.seller_id
-ORDER BY dim_seller_keys.seller_key
+  seller_keys.seller_key,
+  stg_sellers.seller_id,
+  stg_sellers.seller_city,
+  stg_sellers.seller_state,
+  ARRAY_AGG(stg_order_items.order_id) AS order_ids
+FROM {{ ref('stg_sellers') }}
+LEFT JOIN seller_keys
+  ON stg_sellers.seller_id = seller_keys.seller_id
+LEFT JOIN {{ ref('stg_order_items') }}
+  ON stg_sellers.seller_id = stg_order_items.seller_id
+GROUP BY
+  seller_keys.seller_key,
+  stg_sellers.seller_id,
+  stg_sellers.seller_city,
+  stg_sellers.seller_state
+ORDER BY
+  seller_keys.seller_key
