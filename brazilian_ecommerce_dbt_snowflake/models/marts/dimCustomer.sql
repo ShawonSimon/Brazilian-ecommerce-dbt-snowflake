a33ab6fd1a6e11ba2c34747ff8d2008829cfc098
Shawon Simon <shawonsymon@gmail.com>
2025-01-26 14:17:44 +0100

    dim customer final

--- brazilian_ecommerce_dbt_snowflake/models/marts/dimCustomer.sql
+++ brazilian_ecommerce_dbt_snowflake/models/marts/dimCustomer.sql
@@ -1,22 +1,29 @@
-WITH unique_customers AS (
+WITH customer_keys AS (
+    SELECT 
+        customer_id,
+        ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS customer_key
+    FROM {{ ref('stg_customers') }}
+),
+select_customers AS (
     SELECT DISTINCT
-        dim_customer_keys.customer_key,
+        customer_keys.customer_key,
         stg_customers.customer_id,
         stg_customers.customer_unique_id,
         stg_customers.customer_city,
         stg_customers.customer_state,
         FIRST_VALUE(stg_geolocation.geolocation_lat) OVER (
-            PARTITION BY stg_customers.customer_id 
+            PARTITION BY stg_customers.customer_id
             ORDER BY stg_customers.customer_id
         ) AS geolocation_lat,
         FIRST_VALUE(stg_geolocation.geolocation_lng) OVER (
-            PARTITION BY stg_customers.customer_id 
+            PARTITION BY stg_customers.customer_id
             ORDER BY stg_customers.customer_id
         ) AS geolocation_lng
-    FROM {{ ref('stg_customers') }} 
-    LEFT JOIN {{ ref('dim_customer_keys') }} ON stg_customers.customer_id = dim_customer_keys.customer_id 
-    LEFT JOIN {{ ref('stg_geolocation') }} ON stg_customers.customer_zip_code_prefix = stg_geolocation.geolocation_zip_code_prefix 
+    FROM {{ ref('stg_customers') }}
+    LEFT JOIN customer_keys ON stg_customers.customer_id = customer_keys.customer_id
+    LEFT JOIN {{ ref('stg_geolocation') }} ON stg_customers.customer_zip_code_prefix = stg_geolocation.geolocation_zip_code_prefix
 )
 SELECT *
-FROM unique_customers
-ORDER BY customer_key
+FROM select_customers
+ORDER BY customer_key ASC
+
--- brazilian_ecommerce_dbt_snowflake/models/marts/dimProduct.sql
+++ brazilian_ecommerce_dbt_snowflake/models/marts/dimProduct.sql
@@ -1,14 +1,14 @@
 WITH product_keys AS (
-    SELECT
-        product_id,
-        ROW_NUMBER() OVER (ORDER BY product_id) AS product_key
-    FROM {{ ref('stg_products') }}
+ SELECT 
+   product_id,
+   ROW_NUMBER() OVER (ORDER BY product_id) AS product_key
+ FROM {{ ref('stg_products') }}
 )
 SELECT
-    dim_product_keys.product_key,
-    stg_products.product_id,
+    product_keys.product_key,
+    stg_products.product_id, 
     stg_products.product_category_name
-FROM {{ ref('dim_product_keys') }}
-LEFT JOIN {{ ref('stg_products') }} ON
-    dim_product_keys.product_id = stg_products.product_id
-ORDER BY dim_product_keys.product_key
+FROM product_keys
+LEFT JOIN {{ ref('stg_products') }}
+    ON product_keys.product_id = stg_products.product_id
+ORDER BY product_keys.product_key
